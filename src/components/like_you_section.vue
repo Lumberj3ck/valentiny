7ffd2a7778e7157f6df6db2b20164902a5c5173b
unknown <alanuldashev@gmail.com>
2024-02-21 12:19:45 +0100

    new section inputs

--- src/components/like_you_section.vue
+++ src/components/like_you_section.vue
@@ -17,129 +17,113 @@ export default {
     control_bar,
     custom_input
   },
-  computed:{
-    primary_color(){
-        return {
-            "background-color": this.background_color,
-            "color": this.text_color
-        }
+  computed: {
+    primary_color() {
+      return {
+        "background-color": this.background_color,
+        "color": this.text_color
+      }
     }
   },
 
   methods: {
-    onToggleRender() {
-      this.is_render = !this.is_render
-    },
-    onColor_reset(){
-        this.background_color = ''
-        this.text_color = ''
-    },
-    onBgColor_picked(value){
-      this.background_color = value
-    },
-    onText_color_picked(value){
-      this.text_color = value
+    onColor_reset() {
+      this.background_color = ''
+      this.text_color = ''
     },
   }
 }
 </script>
 
 <template>
-<!-- <control_bar @bg_color_reset="onColor_reset" @toggle-render="onToggleRender" @bg_color_picked="onBgColor_picked" @text_color_picked="onText_color_picked"></control_bar> -->
-<control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="onColor_reset" @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value" @text_color_picked="(value) => text_color = value"></control_bar>
-    <section v-if="is_render" class="relative">
-      <div class="px-4 bg_violet" :style="primary_color">
-        <div class="container mx-auto items-center flex flex-wrap">
-          <div
-            class="w-full md:w-4/12 ml-auto mr-auto px-4"
-            style="margin-top: 50px; margin-bottom: 50px"
-          >
-            <img alt="..." class="max-w-full rounded-lg" src="../assets/imgs/heart_img.png" />
-          </div>
-          <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
-            <div class="md:pr-12 mb-8">
-              <custom_input text_area :primary_color="primary_color" class='text-section-color text-3xl font-semibold' default_input_value="I love you so much that:"></custom_input>
-              <ul class="list-none mt-6">
-                <li class="py-2">
-                  <div class="flex items-center">
-                    <div>
-                      <span
-                        class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"
-                      >
-                        <i class="fab fa-google"></i>
-                      </span>
-                    </div>
-                    <div>
-                        <custom_input :primary_color="primary_color" class='text-section-color text-lg ' default_input_value="Can show you my google history"></custom_input>
-                    </div>
+  <control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="onColor_reset"
+    @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value"
+    @text_color_picked="(value) => text_color = value"></control_bar>
+  <section v-if="is_render" class="relative">
+    <div class="px-4 bg_violet" :style="primary_color">
+      <div class="container mx-auto items-center flex flex-wrap">
+        <div class="w-full md:w-4/12 ml-auto mr-auto px-4" style="margin-top: 50px; margin-bottom: 50px">
+          <img alt="..." class="max-w-full rounded-lg" src="../assets/imgs/heart_img.png" />
+        </div>
+        <div class="w-full md:w-5/12 ml-auto mr-auto px-4">
+          <div class="md:pr-12 mb-8">
+            <custom_input text_area :primary_color="primary_color" class='text-section-color text-3xl font-semibold'
+              default_input_value="I love you so much that:"></custom_input>
+            <ul class="list-none mt-6">
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span
+                      class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                      <i class="fab fa-google"></i>
+                    </span>
                   </div>
-                </li>
-                <li class="py-2">
-                  <div class="flex items-center">
-                    <div>
-                      <span
-                        class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"
-                      >
-                        <i class="fab fa-google"></i>
-                      </span>
-                    </div>
-                    <div>
-                     <custom_input :primary_color="primary_color" class='text-section-color text-lg ' default_input_value="Can resist to eat last pizza slice"></custom_input>
-                    </div>
+                  <div>
+                    <custom_input :primary_color="primary_color" class='text-section-color text-lg '
+                      default_input_value="Can show you my google history"></custom_input>
                   </div>
-                </li>
-                <li class="py-2">
-                  <div class="flex items-center">
-                    <div>
-                      <span
-                        class="text-xs font-semibold inline-block py-1 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"
-                        style="padding-right: 0.4rem; padding-left: 0.4rem"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          stroke-width="1.5"
-                          stroke="currentColor"
-                          class="w-4 h-4 text-pink-600"
-                        >
-                          <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                          />
-                        </svg>
-                      </span>
-                    </div>
-                    <div>
-                      <!-- <h4 class="text-section-color">Almost started to like your movie taste</h4> -->
-                     <custom_input :primary_color="primary_color" class='text-section-color text-lg ' default_input_value="Almost started to like your movie taste"></custom_input>
-                    </div>
+                </div>
+              </li>
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span
+                      class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-pink-600 bg-pink-200 mr-3">
+                      <i class="fab fa-google"></i>
+                    </span>
                   </div>
-                </li>
-              </ul>
-            </div>
+                  <div>
+                    <custom_input :primary_color="primary_color" class='text-section-color text-lg '
+                      default_input_value="Can resist to eat last pizza slice"></custom_input>
+                  </div>
+                </div>
+              </li>
+              <li class="py-2">
+                <div class="flex items-center">
+                  <div>
+                    <span
+                      class="text-xs font-semibold inline-block py-1 uppercase rounded-full text-pink-600 bg-pink-200 mr-3"
+                      style="padding-right: 0.4rem; padding-left: 0.4rem">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                        stroke="currentColor" class="w-4 h-4 text-pink-600">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                          d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                      </svg>
+                    </span>
+                  </div>
+                  <div>
+                    <!-- <h4 class="text-section-color">Almost started to like your movie taste</h4> -->
+                    <custom_input :primary_color="primary_color" class='text-section-color text-lg '
+                      default_input_value="Almost started to like your movie taste"></custom_input>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <style>
-.bg_violet{
-    background-color: var(--section-primary-color);
+.bg_violet {
+  background-color: var(--section-primary-color);
 }
 
-.text_violet{
-    color: var(--section-primary-color);
+.text_violet {
+  color: var(--section-primary-color);
 }
-.text-section-color{
+
+.text-section-color {
   color: var(--section-secondary-color)
 }
-w-full{
-    width: 100%;
+
+w-full {
+  width: 100%;
 }
-.select_prevent{
-    user-select: none; 
+
+.select_prevent {
+  user-select: none;
 }
 </style>
--- src/components/reasons_like_you_section.vue
+++ src/components/reasons_like_you_section.vue
@@ -1,5 +1,6 @@
 <script>
 import control_bar from './control_bar.vue'
+import custom_input from './custom_input.vue'
 
 export default {
   data() {
@@ -9,87 +10,79 @@ export default {
       is_render: true,
     }
   },
-  emits:['move_up', 'move_down'],
+  emits: ['move_up', 'move_down'],
   components: {
     control_bar,
-},
+    custom_input
+  },
+  computed:{
+    primary_color(){
+      return {
+        'background-color': this.background_color,
+        'color': this.text_color
+      }
+    }
+  },
+  methods:{
+    onColor_reset(){
+        this.background_color = ''
+        this.text_color = ''
+    },
+  }
 }
 </script>
 
 <template>
-<control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="onColor_reset" @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value" @text_color_picked="(value) => text_color = value"></control_bar>
-      <section class="py-10 bg-white">
-        <div class="container mx-auto px-4">
-          <!-- cards -->
-          <div class="flex flex-wrap items-center">
-            <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
-              <div
-                class="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  class="w-6 h-6"
-                >
-                  <path
-                    d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z"
-                  />
-                </svg>
-              </div>
-              <h3 class="text-3xl mb-2 font-semibold leading-normal text-black">
-                What I like in you
-              </h3>
-              <p class="text-lg font-light leading-relaxed mt-4 mb-4 ">
-              <ol class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-                <li class="font-light">1 Name</li>
-                <li class="font-light">2 Good cooking</li>
-                <li class="font-light">3 Favorite girl</li>
-                <li class="font-light">4 Cute sunn</li>
-                <li class="font-light">5 And couple other things)</li>
-              </ol>
-              </p>
-            </div>
-            <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
-              <div
-                class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-pink-600"
-                style="background-color: var(--card-primary-color)"
-              >
-                <img
-                  alt="..."
-                  src="../assets/imgs/doner.jpg"
-                  class="w-full align-middle rounded-t-lg"
-                />
-                <blockquote class="relative p-8 mb-4">
-                  <svg
-                    preserveAspectRatio="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 583 95"
-                    class="absolute left-0 w-full block"
-                    style="height: 95px; top: -94px"
-                  >
-                    <polygon
-                      points="-30,95 583,95 583,65"
-                      class="text-pink-600 fill-current"
-                      style="color: var(--card-primary-color)"
-                    ></polygon>
-                  </svg>
-                  <h4
-                    class="text-xl font-bold text-white"
-                    style="color: var(--card-secondary-color)"
-                  >
-                  Buys doner for me
-                  </h4>
-                  <p
-                    class="text-md font-light mt-2 text-white"
-                    style="color: var(--card-secondary-color)"
-                  >
-                  Did you know that in a recent study, scientists proved that guys whose girls buy doners are 50% happier?
-                  </p>
-                </blockquote>
-              </div>
-            </div>
+  <control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="onColor_reset"
+    @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value"
+    @text_color_picked="(value) => text_color = value"></control_bar>
+  <section class="py-10 bg-white" :style="primary_color">
+    <div class="container mx-auto px-4">
+      <!-- cards -->
+      <div class="flex flex-wrap items-center">
+        <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
+          <div
+            class="text-red-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-pink-300" :style="primary_color">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+              <path
+                d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+            </svg>
+          </div>
+          <custom_input :primary_color="primary_color" class='text-3xl mb-2 font-semibold leading-normal text-black'
+            default_input_value="What I like in you"></custom_input>
+          <p class="text-lg font-light leading-relaxed mt-4 mb-4 ">
+          <ol class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
+            <custom_input :primary_color="primary_color" class='font-light'
+              default_input_value="1 Name"></custom_input>
+            <custom_input :primary_color="primary_color" class='font-light'
+              default_input_value="2 Good cooking"></custom_input>
+            <custom_input :primary_color="primary_color" class='font-light'
+              default_input_value="3 Favorite girl"></custom_input>
+            <custom_input :primary_color="primary_color" class='font-light'
+              default_input_value="4 Cute sunn"></custom_input>
+            <custom_input :primary_color="primary_color" class='font-light'
+              default_input_value="5 And couple other things)"></custom_input>
+          </ol>
+          </p>
+        </div>
+        <div class="w-full md:w-4/12 px-4 mr-auto ml-auto">
+          <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-pink-600"
+            style="background-color: var(--card-primary-color)">
+            <img alt="..." src="../assets/imgs/doner.jpg" class="w-full align-middle rounded-t-lg" />
+            <blockquote class="relative p-8 mb-4">
+              <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 583 95"
+                class="absolute left-0 w-full block" style="height: 95px; top: -94px">
+                <polygon points="-30,95 583,95 583,65" class="text-pink-600 fill-current"
+                  style="color: var(--card-primary-color)"></polygon>
+              </svg>
+            <custom_input style="color: var(--card-secondary-color)" class='text-xl font-bold text-white'
+              default_input_value="Buys doner for me"></custom_input>
+            <custom_input text_area style="color: var(--card-secondary-color)" class='text-md font-light mt-2 text-white'
+              default_input_value="Did you know that in a recent study, scientists proved that guys whose girls buy doners are 50% happier?"></custom_input>
+            </blockquote>
           </div>
         </div>
-      </section>
+      </div>
+    </div>
+  </section>
 </template>
