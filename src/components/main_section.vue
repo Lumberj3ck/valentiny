29485f8761bf56b8467f3adf3890e492b3ba9b9b
unknown <alanuldashev@gmail.com>
2024-02-23 15:05:05 +0100

    added clarity for moving component

--- src/components/main_section.vue
+++ src/components/main_section.vue
@@ -20,7 +20,7 @@ export default {
         { name: 'love_potion_section', index: 3 },
         { name: 'reasons_like_you_section', index: 4 },
       ],
-      moving_component: ''
+      moving_component: {name: null, direction: null}
     }
   },
   methods: {
@@ -41,55 +41,22 @@ export default {
           }
         })
         this.components[comp_index].index += direction
+        this.moving_component = {name: name, direction: direction}  
       }
-      this.moving_component = [name, direction]
     }
 
   },
-  // computed: {
-  //   animate() {
-  //     return (this.moving_component[0] == this.component.name) && (this.moving_component[1] == -1)
-  //   }
-  // },
-  watch: {
-    moving_component(newValue, oldValue) {
-      if (newValue !== oldValue) {
-        setTimeout(() => {
-          this.moving_component = null;
-        }, 1100);
-      }
-    }
-  }
 }
 </script>
 
 
 <template>
-  <!-- <div class="custom_container">
-    <transition name="fade">
-    <div :style="{ 'grid-row': components[0].index }">
-      <start_section @move_up="move(-1, 'start_section')" @move_down="move(1, 'start_section')"></start_section>
-    </div>
-    </transition>
-    <div :style="{ 'grid-row': components[1].index }">
-      <like_you_section @move_up="move(-1, 'like_you_section')" @move_down="move(1, 'like_you_section')">
-      </like_you_section>
-    </div>
-
-    <div :style="{ 'grid-row': components[2].index }">
-      <love_potion_section @move_up="move(-1, 'love_potion_section')" @move_down="move(1, 'love_potion_section')">
-      </love_potion_section>
-    </div>
-
-    <div :style="{ 'grid-row': components[3].index }">
-      <reasons_like_you_section @move_up="move(-1, 'reasons_like_you_section')"
-        @move_down="move(1, 'reasons_like_you_section')"></reasons_like_you_section>
-    </div>
-  </div> -->
   <div class="custom_container">
     <div v-for="component in components" :key="component.name"
-      :class="{ 'bounce-in-top': (moving_component[0] == component.name) && (moving_component[1] == -1), 'bounce-in-bottom': (moving_component[0] == component.name) && (moving_component[1] == 1) }"
-      :style="{ 'grid-row': component.index }">
+      :class="{ 'slide-in-bck-top': (moving_component.name == component.name) && (moving_component.direction == 1), 'slide-in-bck-bottom': (moving_component.name == component.name) && (moving_component.direction == -1) }"
+      :style="{ 'grid-row': component.index }"
+      @animationend="moving_component = {name: null, direction: null}"
+      >
       <component :is="component.name" @move_up="move(-1, component.name)" @move_down="move(1, component.name)" />
     </div>
   </div>
@@ -109,253 +76,62 @@ export default {
 .second-div {
   grid-row: 2;
 }
-
-.bounce-in-top {
-  -webkit-animation: bounce-in-top 1.1s both;
-  animation: bounce-in-top 1.1s both;
+.slide-in-bck-bottom {
+	-webkit-animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+	        animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
-
-.bounce-in-bottom {
-  -webkit-animation: bounce-in-bottom 1.1s both;
-  animation: bounce-in-bottom 1.1s both;
+.slide-in-bck-top {
+	-webkit-animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+	        animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
-
-@-webkit-keyframes bounce-in-bottom {
+@-webkit-keyframes slide-in-bck-bottom {
   0% {
-    -webkit-transform: translateY(500px);
-    transform: translateY(500px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
+    -webkit-transform: translateZ(700px) translateY(300px);
+            transform: translateZ(700px) translateY(300px);
     opacity: 0;
   }
-
-  38% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
+  100% {
+    -webkit-transform: translateZ(0) translateY(0);
+            transform: translateZ(0) translateY(0);
     opacity: 1;
   }
-
-  55% {
-    -webkit-transform: translateY(65px);
-    transform: translateY(65px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
+}
+@keyframes slide-in-bck-bottom {
+  0% {
+    -webkit-transform: translateZ(700px) translateY(300px);
+            transform: translateZ(700px) translateY(300px);
+    opacity: 0;
   }
-
-  72% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  81% {
-    -webkit-transform: translateY(28px);
-    transform: translateY(28px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  90% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  95% {
-    -webkit-transform: translateY(8px);
-    transform: translateY(8px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
   100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
+    -webkit-transform: translateZ(0) translateY(0);
+            transform: translateZ(0) translateY(0);
+    opacity: 1;
   }
 }
 
-@keyframes bounce-in-bottom {
+@-webkit-keyframes slide-in-bck-top {
   0% {
-    -webkit-transform: translateY(500px);
-    transform: translateY(500px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
+    -webkit-transform: translateZ(700px) translateY(-300px);
+            transform: translateZ(700px) translateY(-300px);
     opacity: 0;
   }
-
-  38% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
+  100% {
+    -webkit-transform: translateZ(0) translateY(0);
+            transform: translateZ(0) translateY(0);
     opacity: 1;
   }
-
-  55% {
-    -webkit-transform: translateY(65px);
-    transform: translateY(65px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
+}
+@keyframes slide-in-bck-top {
+  0% {
+    -webkit-transform: translateZ(700px) translateY(-300px);
+            transform: translateZ(700px) translateY(-300px);
+    opacity: 0;
   }
-
-  72% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  81% {
-    -webkit-transform: translateY(28px);
-    transform: translateY(28px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  90% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  95% {
-    -webkit-transform: translateY(8px);
-    transform: translateY(8px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
   100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
+    -webkit-transform: translateZ(0) translateY(0);
+            transform: translateZ(0) translateY(0);
+    opacity: 1;
   }
 }
 
-@-webkit-keyframes bounce-in-top {
-  0% {
-    -webkit-transform: translateY(-500px);
-    transform: translateY(-500px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-    opacity: 0;
-  }
-
-  38% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-    opacity: 1;
-  }
-
-  55% {
-    -webkit-transform: translateY(-65px);
-    transform: translateY(-65px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  72% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  81% {
-    -webkit-transform: translateY(-28px);
-    transform: translateY(-28px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  90% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  95% {
-    -webkit-transform: translateY(-8px);
-    transform: translateY(-8px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-}
-
-@keyframes bounce-in-top {
-  0% {
-    -webkit-transform: translateY(-500px);
-    transform: translateY(-500px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-    opacity: 0;
-  }
-
-  38% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-    opacity: 1;
-  }
-
-  55% {
-    -webkit-transform: translateY(-65px);
-    transform: translateY(-65px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  72% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  81% {
-    -webkit-transform: translateY(-28px);
-    transform: translateY(-28px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  90% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-
-  95% {
-    -webkit-transform: translateY(-8px);
-    transform: translateY(-8px);
-    -webkit-animation-timing-function: ease-in;
-    animation-timing-function: ease-in;
-  }
-
-  100% {
-    -webkit-transform: translateY(0);
-    transform: translateY(0);
-    -webkit-animation-timing-function: ease-out;
-    animation-timing-function: ease-out;
-  }
-}</style>
+</style>
