3067bd562fbc1afca1908f59b5487ca254b28e35
unknown <alanuldashev@gmail.com>
2024-02-23 14:27:03 +0100

    animations for section moving

---
+++ src/components/start_section.vue
@@ -0,0 +1,78 @@
+<script>
+import control_bar from './control_bar.vue'
+import custom_input from './custom_input.vue'
+import useControlBar from '../js/control_bar.js'
+
+export default {
+    setup() {
+        const { background_color, text_color, is_render, primary_color, resetColors } = useControlBar();
+
+        return {
+            background_color,
+            text_color,
+            is_render,
+            primary_color,
+            resetColors
+        };
+    },
+    emits: ['move_up', 'move_down'],
+    components: {
+        control_bar,
+        custom_input
+    },
+    computed: {
+        primary_text_color() {
+            return {
+                'color': this.text_color
+            }
+        }
+    }
+}
+</script>
+
+
+<template >
+    <control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="resetColors"
+        @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value"
+        @text_color_picked="(value) => text_color = value"></control_bar>
+    <div v-if="is_render">
+        <div class="relative pt-16 pb-32 flex content-center items-center justify-center" style="min-height: 75vh">
+            <div class="absolute top-0 w-full h-full bg-center main_banner bg-cover md:bg-contain"
+                style="background-image: url('./src/assets/imgs/banner.jpg')">
+                <span id="blackOverlay" class="w-full h-full absolute bg-black opacity-[0.5]" :style="primary_color"></span>
+            </div>
+
+            <div class="container relative mx-auto">
+                <div class="items-center flex flex-wrap">
+                    <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                        <div class="md:pr-12 text-[#f9d0d7]">
+                            <custom_input :primary_color="primary_text_color" class='font-semibold text-3xl md:text-5xl'
+                                default_input_value="Congratulations!"></custom_input>
+                            <!-- <h1 class="font-semibold text-5xl">
+                                Congratulations!
+                            </h1> -->
+                            <custom_input text_area :primary_color="primary_text_color" class='mt-4 text-lg'
+                                default_input_value="Today is Valentine's Day, and I don't want to pressure you, but you should already plan how you confess your love to me ">
+                            </custom_input>
+                            <!-- <p class="mt-4 text-lg">
+                                Today is Valentine's Day, and I don't want to pressure you, but you should already plan how
+                                you confess your love to me
+                                <i class="far fa-face-kiss-wink-heart" style="color: #f8d0d7"></i>
+                            </p> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+                style="height: 70px">
+                <svg class="absolute overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none"
+                    version="1.1" viewBox="0 0 2560 100" x="0" y="0" style="bottom: -1px">
+                    <polygon class="text-white fill-current" points="2560 0 2560 100 0 100"></polygon>
+                </svg>
+            </div> -->
+        </div>
+    </div>
+</template>
+
+
+<style></style>
--- src/js/page_download.js
+++ src/js/page_download.js
@@ -2,13 +2,13 @@ import * as cheerio from 'cheerio';
 import JSZip from 'jszip';
 import { saveAs } from 'file-saver';
 
-function remove_redudant_ui(html){
+function remove_redudant_ui(html) {
     html('.control_bar').remove();
     html('script').remove();
     html('#get_html').remove();
 }
 
-function make_links_relative($){
+function make_links_relative($) {
     $('img').each((index, el) => { $(el).attr('src', `.${$(el).attr('src')}`) });
     $('link').each((index, el) => {
         if (!$(el).attr('href').startsWith('http')) {
@@ -17,13 +17,13 @@ function make_links_relative($){
     });
 }
 
-function instantiate_dom(){
+function instantiate_dom() {
     var dom = document.documentElement.innerHTML;
     const $ = cheerio.load(dom);
     return $
 }
 
-function mutate_html(){
+function mutate_html() {
     var $ = instantiate_dom()
     remove_redudant_ui($)
     make_links_relative($)
@@ -31,7 +31,16 @@ function mutate_html(){
     return $
 }
 
-function get_resource_links(){
+function get_css_url_resourses(relativeResources){
+    const elementsWithBackgroundImage = document.querySelectorAll('[style*="background-image"]');
+    elementsWithBackgroundImage.forEach(element => {
+        const styleAttributeValue = element.getAttribute('style');
+        const url = styleAttributeValue.match(/url\(['"]?([^'"]+)['"]?\)/)[1];
+        relativeResources.push(url);
+    });
+}
+
+function get_resource_links() {
     const relativeResources = [];
     document.querySelectorAll('img, link').forEach(element => {
         const url = element.getAttribute('src') || element.getAttribute('href');
@@ -39,15 +48,17 @@ function get_resource_links(){
             relativeResources.push(url);
         }
     });
+    get_css_url_resourses(relativeResources)
     return relativeResources
 }
 
-function make_full_links(relativeResources, baseUrl){
-    const fullRelativeUrls = relativeResources.map(url => baseUrl + url);
+function make_full_links(relativeResources, baseUrl) {
+    const fullRelativeUrls = relativeResources.map(url => { return url.startsWith('.') ? baseUrl + url.slice(1) :baseUrl + url});
+    console.log(fullRelativeUrls)
     return fullRelativeUrls
 }
 
-async function get_resourses(fullRelativeUrls, dom, baseUrl){
+async function get_resourses(fullRelativeUrls, dom, baseUrl) {
     var zip = JSZip()
     zip.file('index.html', dom.html())
     await Promise.all(fullRelativeUrls.map(async url => {
@@ -59,7 +70,7 @@ async function get_resourses(fullRelativeUrls, dom, baseUrl){
     return zip
 }
 
-function download_archive(zip){
+function download_archive(zip) {
     zip.generateAsync({ type: "blob" })
         .then(function (content) {
             saveAs(content, "Postcard.zip");
@@ -78,4 +89,4 @@ async function get_page() {
     download_archive(zip)
 }
 
-export {get_page, remove_redudant_ui, instantiate_dom}
+export { get_page, remove_redudant_ui, instantiate_dom }
