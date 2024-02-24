e345c4fc68f210e12d0fdee4c02d7f77a36337e8
unknown <alanuldashev@gmail.com>
2024-02-25 00:17:15 +0100

    new design interface

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,13 @@ import main_section from './components/main_section.vue'
 import {get_page, remove_redudant_ui, instantiate_dom} from './js/page_download.js'
 // import VueHtml2pdf from 'vue-html2pdf'
 import html2pdf from 'html2pdf.js'
+import download_button from './components/button_download_html.vue'
 
 
 export default {
   components: {
-    main_section
+    main_section,
+    download_button
   },
 
   methods: {
@@ -36,10 +38,11 @@ export default {
 </script>
 
 <template>
-  <div id="get_html">
+  <!-- <div id="get_html">
   <button style="margin-right: 20px;" @click="exportToPDF">Export to pdf</button>
   <button @click="get_html">Get html</button>
-</div>
+</div> -->
+  <download_button></download_button>
   <main_section></main_section>
   <!-- <VueHtml2pdf :show-layout="false" :float-layout="true" :enable-download="true" :preview-modal="true"
     :paginate-elements-by-height="1400" filename="myPDF" :pdf-quality="2" :manual-pagination="false" pdf-format="a4"
--- src/components/control_bar.vue
+++ src/components/control_bar.vue
@@ -1,58 +1,155 @@
 <script>
 
-export default{
-    emits:['move_down', 'move_up','toggle-render', 'bg_color_reset', 'bg_color_picked', 'text_color_picked'],
+export default {
+    emits: ['move_down', 'move_up', 'toggle-render', 'bg_color_reset', 'bg_color_picked', 'text_color_picked'],
 }
 </script>
 
 
 <template>
-<div class="flex space_around  bg_white text_black control_bar">
-    <div class="flex col_25">
-  <button @click="$emit('toggle-render')" class="button_simple">Toggle Render</button>
-  <button @click="$emit('bg_color_reset')" class="button_simple">Reset</button>
-</div>
-  <div class="flex col_25 items-center">
-  <svg @click="$emit('move_down')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w4 h4"><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
-  <svg @click="$emit('move_up')" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512" class="w4 h4"><path d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"/></svg>
-  <input type="color" @input="$emit('bg_color_picked', $event.target.value)" class="square_input">
-  <input type="color" @input="$emit('text_color_picked', $event.target.value)" class="square_input">
-   </div>
-</div>
+    <div class="flex bg_white text_black control_bar justify-start gap-4 ml-3 my-1 sm:justify-center md:justify-around">
+        <div class="flex items-center gap-4 md:gap-10">
+            <!-- <button @click="$emit('toggle-render')" class="button_simple">Toggle Render</button> -->
+            <i @click="$emit('toggle-render')" class="fas fa-power-off control_item"></i>
+            <button @click="$emit('bg_color_reset')" class="button_simple">Reset</button>
+        </div>
+        <div class="flex items-center gap-4 md:gap-10">
+            <i  @click="$emit('move_down')" class="fa-solid fa-arrow-down control_item"></i>
+            <i  @click="$emit('move_up')" class="fa-solid fa-arrow-up control_item"></i>
+        </div>
+        <div class="flex items-center gap-4 md:gap-10">
+            <input id='style1' type="color" @input="$emit('bg_color_picked', $event.target.value)" class="custom_input">
+            <input type="color" @input="$emit('text_color_picked', $event.target.value)" class="custom_input">
+        </div>
+    </div>
 </template>
 
 <style>
-    .w4{
-        width: 30px;
+.button_simple {
+  --color: #FFDE81;
+  font-family: inherit;
+  display: inline-block;
+  width: 5em;
+  height: 2.6em;
+  line-height: 2.5em;
+  overflow: hidden;
+  cursor: pointer;
+  /* margin: 20px; */
+  font-size: 17px;
+  z-index: 1;
+  color: black;
+  border: 2px solid black;
+  border-radius: 6px;
+  position: relative;
+}
+
+.button_simple::before {
+  position: absolute;
+  content: "";
+  background: var(--color);
+  width: 150px;
+  height: 200px;
+  z-index: -1;
+  border-radius: 50%;
+}
+
+.control_item{
+    font-size: 25px;
+    cursor: pointer;
+    font-weight: 900;
+}
+
+.control_item:hover{
+    color: #FFDE81;
+}
+
+@media (min-width: 640px){
+    .control_item{
+        font-size: 27px;
     }
-    .h4{
-        height: 30px;
+}
+
+@media (min-width: 768px){
+    .control_item{
+        font-size: 30px;
     }
-    .square_input{
-        width: 40px;
-        height: 40px;
-    }
-    .button_simple{
-        border: 1px solid black;
-        border-radius: 10px;
-        padding: 4px;
-    }
-    .bg_white{
-        background-color: white;
-    }
-    .text_black{
-        color: black;
-    }
-    .mb_24{
-        margin-bottom: 24px;
-    }
-    .flex{
-        display: flex;
-    }
-    .space_around{
-        justify-content: space-around;
-    }
-    .col_25{
-        column-gap: 25px;
-    }
+}
+.button_simple:hover {
+  color: black;
+}
+
+.button_simple:before {
+  top: 100%;
+  left: 100%;
+  transition: 0.3s all;
+}
+
+.button_simple:hover::before {
+  top: -30px;
+  left: -30px;
+}
+
+
+.arrow {
+    cursor: pointer;
+}
+.arrow:hover{
+    color: green; 
+}
+/* .arrow:hover{
+    color: var(--button_background)
+} */
+.w4 {
+    width: 30px;
+}
+
+.h4 {
+    height: 30px;
+}
+
+.custom_input {
+    width: 40px;
+    height: 40px;
+}
+
+.custom_input::-webkit-color-swatch {
+    border-radius: 7px;
+    border: none;
+    cursor: pointer;
+}
+
+.custom_input::-moz-color-swatch {
+    border-radius: 7px;
+    border: none;
+}
+
+/* .button_simple {
+    border: 1px solid black;
+    border-radius: 10px;
+    padding: 4px;
+} */
+
+.bg_white {
+    background-color: white;
+}
+
+.text_black {
+    color: black;
+}
+
+.mb_24 {
+    margin-bottom: 24px;
+}
+
+.flex {
+    display: flex;
+}
+
+.space_around {
+    justify-content: space-around;
+}
+
+.col_25 {
+    column-gap: 25px;
+}
 </style>
