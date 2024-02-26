09de2bfd50ca8707e93083ca1625aa70b094696f
unknown <alanuldashev@gmail.com>
2024-02-26 16:19:57 +0100

    new feature uploading user input picture

--- src/components/start_section.vue
+++ src/components/start_section.vue
@@ -41,7 +41,7 @@ export default {
     <control_bar @move_up="$emit('move_up')" @move_down="$emit('move_down')" @bg_color_reset="resetColors"
         @toggle-render="is_render = !is_render" @bg_color_picked="(value) => background_color = value"
         @text_color_picked="(value) => text_color = value"></control_bar>
-    <div v-if="is_render">
+    <section v-if="is_render">
         <div class="relative pt-16 pb-32 flex content-center items-center justify-center" style="min-height: 75vh">
             <div class="absolute top-0 w-full h-full bg-center bg-cover md:bg-contain"
                 :style="{'background-image': `url(${banner1})`}">
@@ -70,7 +70,7 @@ export default {
                 </div>
             </div>
         </div>
-    </div>
+    </section>
 </template>
 
 
--- src/js/page_download.js
+++ src/js/page_download.js
@@ -7,10 +7,21 @@ function remove_redudant_ui(html) {
     html('.system_ui').remove();
     html('script').remove();
     html('#get_html').remove();
+    html('.image_input').remove();
 }
 
 function make_links_relative($) {
-    $('img').each((index, el) => { $(el).attr('src', `.${$(el).attr('src')}`) });
+    $('img').each((index, el) => {
+        var src = $(el).attr('src')
+        if (!src.startsWith('data:')) {
+            $(el).attr('src', `.${src}`)
+        }
+        else{
+            // var file_type = src.match('data:/([a-zA-Z]+);') 
+            // var data_img_name = `./src/assets/imgs/user_input_${index}.${file_type}`
+            $(el).attr('src', $(el).attr('data-verbose-path'))
+        }
+    });
     $('link').each((index, el) => {
         if (!$(el).attr('href').startsWith('http')) {
             $(el).attr('href', `.${$(el).attr('href')}`);
@@ -32,40 +43,64 @@ function mutate_html() {
     return $
 }
 
-function get_css_url_resourses(relativeResources){
+function make_full_link(path, baseUrl){
+    return path.startsWith('@') || path.startsWith('.') ? baseUrl + path.slice(1) : baseUrl + path 
+}
+
+function get_css_url_resourses(resources, baseUrl) {
     const elementsWithBackgroundImage = document.querySelectorAll('[style*="background-image"]');
     elementsWithBackgroundImage.forEach(element => {
         const styleAttributeValue = element.getAttribute('style');
         const url = styleAttributeValue.match(/url\(['"]?([^'"]+)['"]?\)/)[1];
-        relativeResources.push(url);
+        const full_url = make_full_link(url, baseUrl)
+        resources.push({url: full_url, verbose_url: url});
     });
 }
 
-function get_resource_links() {
-    const relativeResources = [];
+function get_resource_links(baseUrl) {
+    const resources = [];
     document.querySelectorAll('img, link').forEach(element => {
         const url = element.getAttribute('src') || element.getAttribute('href');
-        if (url && !url.startsWith('http') && !url.startsWith('data:')) {
-            relativeResources.push(url);
+        if (url && !url.startsWith('http')) {
+            // var full_url = url.startsWith('@') || url.startsWith('.') ? baseUrl + url.slice(1) : baseUrl + url
+            var full_url = make_full_link(url, baseUrl)
+            url.startsWith('data:') ? resources.push({url: url, verbose_url:element.getAttribute('data-verbose-path')}) : resources.push({url: full_url, verbose_url: url})
         }
     });
-    get_css_url_resourses(relativeResources)
-    return relativeResources
+    get_css_url_resourses(resources, baseUrl)
+    return resources 
 }
 
-function make_full_links(relativeResources, baseUrl) {
-    const fullRelativeUrls = relativeResources.map(url => { return url.startsWith('@') ? baseUrl + url.slice(1) :baseUrl + url});
-    return fullRelativeUrls
-}
+// function make_full_links(relativeResources, baseUrl) {
+//     const fullRelativeUrls = relativeResources.map(el => {
+//         // return url.startsWith('@') ? baseUrl + url.slice(1) :baseUrl + url
+//         if (el.url.startsWith('@') || el.url.startsWith('.')) {
+//             return baseUrl + el.url.slice(1)
+//         }
+//         else if (el.url.startsWith('data:')) {
+//             return el.url
+//         }
+//         else {
+//             return baseUrl + el.url
+//         }
 
-async function get_resourses(fullRelativeUrls, dom, baseUrl) {
+//     });
+//     console.log(fullRelativeUrls)
+//     return fullRelativeUrls
+// }
+
+async function get_resourses(fullRelativeUrls, dom) {
     var zip = JSZip()
     zip.file('index.html', dom.html())
     await Promise.all(fullRelativeUrls.map(async url => {
-        const response = await fetch(url);
+        console.log(url)
+        const response = await fetch(url.url);
         const blob = await response.blob();
-        const relativePath = url.replace(`${baseUrl}/`, '');
-        zip.file(relativePath, blob, { binary: true });
+        // var relativePath = url.replace(`${baseUrl}/`, '');
+        // if (url.startsWith('data:')) {
+        //     relativePath = 'some_user_input.jpeg'
+        // }
+        zip.file(url.verbose_url, blob, { binary: true });
     }));
     return zip
 }
@@ -78,14 +113,14 @@ function download_archive(zip) {
 }
 
 async function get_page() {
-    const relativeResources = get_resource_links()
+    const baseUrl = window.location.origin;
+    const resourse_links = get_resource_links(baseUrl)
     var $ = mutate_html()
 
 
-    const baseUrl = window.location.origin;
-    const fullRelativeUrls = make_full_links(relativeResources, baseUrl)
-
-    var zip = await get_resourses(fullRelativeUrls, $, baseUrl)
+    // const fullRelativeUrls = make_full_links(relativeResources, baseUrl)
+    
+    var zip = await get_resourses(resourse_links, $)
     download_archive(zip)
 }
 
