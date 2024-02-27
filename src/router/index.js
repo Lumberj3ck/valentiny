b0b233b0c9f3204a8b71279aee54fbcaca043014
unknown <alanuldashev@gmail.com>
2024-02-27 17:35:40 +0100

    new landing

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MainSection from '@/components/main_section.vue'
 import LandingPage from '@/components/landing_page.vue'
+import LandingPage1 from '@/components/landing_page1.vue'
 
 
 const routes = [
     {
         path: '/', 
         // name: 'Home', 
+        // component: LandingPage, 
+        component: LandingPage1, 
+    },
+    {
+        path: '/1/', 
+        // name: 'Home', 
+        // component: LandingPage, 
         component: LandingPage, 
     },
     {
