d50cad9c576e350da202849dfbe358de48d636cf
unknown <alanuldashev@gmail.com>
2024-02-27 12:55:33 +0100

    simple navbar and photomode switch

--- src/components/button_download_html.vue
+++ src/components/button_download_html.vue
@@ -1,6 +1,6 @@
 <script>
 import { get_page } from '@/js/page_download'
-import * as htmlToImage from 'html-to-image';
+// import * as htmlToImage from 'html-to-image';
 // import { instantiate_dom, remove_redudant_ui } from '@/js/page_download';
 
 export default {
@@ -17,19 +17,19 @@ export default {
         get_html() {
             get_page()
         },
-        generateReport() {
-            function filter(node){
-                // console.log(node)
-                return (node.classList ? !node.classList.contains( 'control_bar') && !node.classList.contains('system_ui') : true)
-            }
-            htmlToImage.toJpeg(document.querySelector('#app'), { quality: 0.95, filter: filter })
-                .then(function (dataUrl) {
-                    var link = document.createElement('a');
-                    link.download = 'valentine-postcard.jpeg';
-                    link.href = dataUrl;
-                    link.click();
-                });
-        }
+        // generateReport() {
+        //     function filter(node){
+        //         // console.log(node)
+        //         return (node.classList ? !node.classList.contains( 'control_bar') && !node.classList.contains('system_ui') : true)
+        //     }
+        //     htmlToImage.toJpeg(document.querySelector('#app'), { quality: 0.95, filter: filter })
+        //         .then(function (dataUrl) {
+        //             var link = document.createElement('a');
+        //             link.download = 'valentine-postcard.jpeg';
+        //             link.href = dataUrl;
+        //             link.click();
+        //         });
+        // }
 
     }
 }
@@ -38,10 +38,11 @@ export default {
 
 
 <template>
-    <div :class="{ 'wobble-ver-right': animate }"
-        class="text-2xl fixed bottom-2 right-2 min-[540px]:bottom-6 min-[540px]:right-6 z-10 text-black cursor-pointer  flex justify-center items-center w-21 h-11 w bg-[#eec249] md:w-14 md:h-14 md:text-3xl system_ui">
+    <!-- <div :class="{ 'wobble-ver-right': animate }"  class="text-2xl fixed bottom-2 right-2 min-[540px]:bottom-6 min-[540px]:right-6 z-10 text-black cursor-pointer  flex justify-center items-center w-11 h-11 w bg-[#eec249] md:w-14 md:h-14 md:text-3xl system_ui rounded-full"> -->
+    <div class="flex text-2xl">
+        <h1 class="font-semibold text-base mr-2">Download Page:</h1>
         <i @click="get_html" class="fa-solid fa-download"></i>
-        <button @click="generateReport" class="system_ui">Get image</button>
+        <!-- <button @click="generateReport" class="system_ui">Get image</button> -->
     </div>
 </template>
 
--- src/components/main_section.vue
+++ src/components/main_section.vue
@@ -22,7 +22,8 @@ export default {
         { name: 'love_potion_section', index: 3 },
         { name: 'reasons_like_you_section', index: 4 },
       ],
-      moving_component: {name: null, direction: null},
+      moving_component: { name: null, direction: null },
+      photoMode: false
     }
   },
   methods: {
@@ -43,7 +44,7 @@ export default {
           }
         })
         this.components[comp_index].index += direction
-        this.moving_component = {name: name, direction: direction}  
+        this.moving_component = { name: name, direction: direction }
       }
     }
 
@@ -53,21 +54,101 @@ export default {
 
 
 <template>
-  <!-- <input type="checkbox" v-model="photoMode"> -->
-  <download_button ></download_button>
+  <div class="flex justify-start sm:justify-center m-3">
+  <div class="flex gap-5 flex-col rounded-lg border-black border-2 p-2 w-5/6">
+    <div class="flex">
+    <h1 class="font-semibold mr-2">Photo Mode:</h1>
+    <label class="toggle-switch">
+      <input type="checkbox" v-model="photoMode">
+      <div class="toggle-switch-background">
+        <div class="toggle-switch-handle"></div>
+      </div>
+    </label>
+    </div>
+    <download_button></download_button>
+  </div>
+</div>
   <div class="custom_container">
     <div v-for="component in components" :key="component.name"
       :class="{ 'slide-in-bck-top': (moving_component.name == component.name) && (moving_component.direction == 1), 'slide-in-bck-bottom': (moving_component.name == component.name) && (moving_component.direction == -1) }"
-      :style="{ 'grid-row': component.index }"
-      @animationend="moving_component = {name: null, direction: null}"
-      >
-      <component :is="component.name" @move_up="move(-1, component.name)" @move_down="move(1, component.name)"/>
+      :style="{ 'grid-row': component.index }" @animationend="moving_component = { name: null, direction: null }">
+      <component :is="component.name" @move_up="move(-1, component.name)" @move_down="move(1, component.name)"
+        :photoMode="photoMode" />
     </div>
   </div>
 </template>
 
 
 <style>
+.toggle-switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 30px;
+  cursor: pointer;
+}
+
+.toggle-switch input[type="checkbox"] {
+  display: none;
+}
+
+.toggle-switch-background {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
+  border-radius: 20px;
+  box-shadow: inset 0 0 0 2px #ccc;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.toggle-switch-handle {
+  position: absolute;
+  top: 5px;
+  left: 5px;
+  width: 20px;
+  height: 20px;
+  background-color: #eec249;
+  border-radius: 50%;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  transition: transform 0.3s ease-in-out;
+}
+
+.toggle-switch::before {
+  content: "";
+  position: absolute;
+  top: -25px;
+  right: -35px;
+  font-size: 12px;
+  font-weight: bold;
+  color: #aaa;
+  text-shadow: 1px 1px #fff;
+  transition: color 0.3s ease-in-out;
+}
+
+.toggle-switch input[type="checkbox"]:checked+.toggle-switch-handle {
+  transform: translateX(45px);
+  box-shadow: 0 2px 5px rgb(255, 255, 255), 0 0 0 3px #05c46b;
+}
+
+.toggle-switch input[type="checkbox"]:checked+.toggle-switch-background {
+  background-color: #eec249;
+  box-shadow: inset 0 0 0 2px #ffffff;
+}
+
+.toggle-switch input[type="checkbox"]:checked+.toggle-switch:before {
+  content: "On";
+  color: #05c46b;
+  right: -15px;
+}
+
+.toggle-switch input[type="checkbox"]:checked+.toggle-switch-background .toggle-switch-handle {
+  transform: translateX(30px);
+  background-color: black;
+}
+
 .custom_container {
   display: grid;
   grid-template-rows: auto 1fr;
@@ -80,35 +161,41 @@ export default {
 .second-div {
   grid-row: 2;
 }
+
 .slide-in-bck-bottom {
-	-webkit-animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-	        animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+  -webkit-animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+  animation: slide-in-bck-bottom 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
+
 .slide-in-bck-top {
-	-webkit-animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-	        animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+  -webkit-animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
+  animation: slide-in-bck-top 0.6s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
 }
+
 @-webkit-keyframes slide-in-bck-bottom {
   0% {
     -webkit-transform: translateZ(700px) translateY(300px);
-            transform: translateZ(700px) translateY(300px);
+    transform: translateZ(700px) translateY(300px);
     opacity: 0;
   }
+
   100% {
     -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
+    transform: translateZ(0) translateY(0);
     opacity: 1;
   }
 }
+
 @keyframes slide-in-bck-bottom {
   0% {
     -webkit-transform: translateZ(700px) translateY(300px);
-            transform: translateZ(700px) translateY(300px);
+    transform: translateZ(700px) translateY(300px);
     opacity: 0;
   }
+
   100% {
     -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
+    transform: translateZ(0) translateY(0);
     opacity: 1;
   }
 }
@@ -116,26 +203,27 @@ export default {
 @-webkit-keyframes slide-in-bck-top {
   0% {
     -webkit-transform: translateZ(700px) translateY(-300px);
-            transform: translateZ(700px) translateY(-300px);
+    transform: translateZ(700px) translateY(-300px);
     opacity: 0;
   }
+
   100% {
     -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
-    opacity: 1;
-  }
-}
-@keyframes slide-in-bck-top {
-  0% {
-    -webkit-transform: translateZ(700px) translateY(-300px);
-            transform: translateZ(700px) translateY(-300px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateZ(0) translateY(0);
-            transform: translateZ(0) translateY(0);
+    transform: translateZ(0) translateY(0);
     opacity: 1;
   }
 }
 
-</style>
+@keyframes slide-in-bck-top {
+  0% {
+    -webkit-transform: translateZ(700px) translateY(-300px);
+    transform: translateZ(700px) translateY(-300px);
+    opacity: 0;
+  }
+
+  100% {
+    -webkit-transform: translateZ(0) translateY(0);
+    transform: translateZ(0) translateY(0);
+    opacity: 1;
+  }
+}</style>
