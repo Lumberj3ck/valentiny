b77e0aace267988033a2f1ce07fa2dc34933935f
unknown <alanuldashev@gmail.com>
2024-02-28 20:58:47 +0100

    small bug fix

--- src/components/love_potion_section.vue
+++ src/components/love_potion_section.vue
@@ -37,7 +37,6 @@ export default{
         reader.onload = () => {
           this.image_url  = reader.result;
           var file_type = this.image_url.match('data:image/([a-zA-Z]+);')[1]
-          // var data_img_name = `./src/assets/imgs/user_input_${index}.${file_type}`
           this.$refs.img.setAttribute("data-verbose-path",`./assets/imgs/user_input_love_potion.${file_type}`)
         };
         reader.readAsDataURL(file);
