6bf64d11dfc1218cd0456376aaba8844df9d6db8
unknown <alanuldashev@gmail.com>
2024-02-28 17:32:48 +0100

    relative path for user input image

--- src/components/love_potion_section.vue
+++ src/components/love_potion_section.vue
@@ -38,7 +38,7 @@ export default{
           this.image_url  = reader.result;
           var file_type = this.image_url.match('data:image/([a-zA-Z]+);')[1]
           // var data_img_name = `./src/assets/imgs/user_input_${index}.${file_type}`
-          this.$refs.img.setAttribute("data-verbose-path",`assets/imgs/user_input_love_potion.${file_type}`)
+          this.$refs.img.setAttribute("data-verbose-path",`./assets/imgs/user_input_love_potion.${file_type}`)
         };
         reader.readAsDataURL(file);
       }
