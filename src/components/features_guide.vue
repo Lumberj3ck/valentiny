8b81dbf5a5a42a9c9702eb62b607203c8de509dd
unknown <alanuldashev@gmail.com>
2024-02-28 11:57:27 +0100

    features guide style

--- src/components/features_guide.vue
+++ src/components/features_guide.vue
@@ -1,7 +1,7 @@
 <script>
 export default{
     mounted(){
-        document.querySelector('body').style.backgroundColor = '#000000'
+        document.querySelector('body').style.backgroundColor = '#242424'
     },
     unmounted(){
         document.querySelector('body').style.backgroundColor = 'inherit'
@@ -11,53 +11,52 @@ export default{
 </script>
 
 <template>
-    <div class="component_wrapper">
     <div class="container">
         <h1>Welcome to the Page Editor!</h1>
-        <p class="text-gray-600">When you first load the page editor, you'll have several options for customizing your
+        <p class="text-white">When you first load the page editor, you'll have several options for customizing your
             template:</p>
-        <ol>
-            <li><strong>Edit Text:</strong>
-                <div class="text-gray-600">Click on any text block to start editing it. Simply click on the text you want to
+        <ol class="text-white">
+            <li><strong class="text-yellow">Edit Text:</strong>
+                <div class="">Click on any text block to start editing it. Simply click on the text you want to
                     modify, and start typing.</div>
             </li>
 
-            <li><strong>Choose Template Picture:</strong>
-                <div class="text-gray-600">Click on the <strong>"Choose"</strong> button to select a template picture for
+            <li><strong class="text-yellow">Choose Template Picture:</strong>
+                <div class="">Click on the <strong>"Choose"</strong> button to select a template picture for
                     your postcard. Browse and choose the one that best fits your message and style.</div>
             </li>
 
-            <li><strong>Color Pickers:</strong>
-                <div class="text-gray-600"> Use the color pickers to customize the background color and text color of your
+            <li><strong class="text-yellow">Color Pickers:</strong>
+                <div class=""> Use the color pickers to customize the background color and text color of your
                     postcard. Click on the color picker icons to open the color selection panel, and choose your desired
                     colors.</div>
             </li>
 
-            <li><strong>Rearrange Blocks:</strong>
-                <div class="text-gray-600">Click on the arrows to rearrange the blocks inside your postcard.</div>
+            <li><strong class="text-yellow">Rearrange Blocks:</strong>
+                <div class="">Click on the arrows to rearrange the blocks inside your postcard.</div>
             </li>
 
-            <li><strong>Reset Button:</strong>
-                <div class="text-gray-600">Use the <strong>"Reset"</strong> button to reset any changes you've made to the
+            <li><strong class="text-yellow">Reset Button:</strong>
+                <div class="">Use the <strong>"Reset"</strong> button to reset any changes you've made to the
                     picked colors and image. Clicking this button will revert the background color, text color, and template
                     picture back to their original settings.</div>
             </li>
 
-            <li><strong>Turn Off Button:</strong>
-                <div class="text-gray-600">Toggle the <strong>"Reset"</strong> button to hide or disable the current block.
+            <li><strong class="text-yellow">Turn Off Button:</strong>
+                <div class="">Toggle the <strong>"Reset"</strong> button to hide or disable the current block.
                     This allows you to temporarily remove a block from your postcard without deleting it entirely.</div>
             </li>
         </ol>
      </div>   
- </div>
 </template>
 
 <style scoped>
-.component_wrapper{
-    background-color: black;
-    height: 100%;
+:root{
+   --secondary-yellow: #eec249; 
 }
-
+.text-yellow{
+    color: #eec249; 
+}
 .container {
     max-width: 900px;
     margin: 0 auto;
@@ -65,7 +64,8 @@ export default{
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    background-color: black;
+    background-color: #242424;
+    /* background-color: #1f1010; */
     color: #eec249; 
 }
 
