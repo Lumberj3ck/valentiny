f684d4eafb623763059f0db2f27af546eb1c3cdf
unknown <alanuldashev@gmail.com>
2024-02-28 20:58:47 +0100

    small bug fix

--- src/components/features_guide.vue
+++ src/components/features_guide.vue
@@ -1,49 +1,49 @@
 <script>
-export default{
-    mounted(){
-        document.querySelector('body').style.backgroundColor = '#242424'
-    },
-    unmounted(){
-        document.querySelector('body').style.backgroundColor = 'inherit'
-    }
-}
+// export default{
+//     mounted(){
+//         document.querySelector('body').style.backgroundColor = '#242424'
+//     },
+//     unmounted(){
+//         document.querySelector('body').style.backgroundColor = 'inherit'
+//     }
+// }
 
 </script>
 
 <template>
     <div class="container">
-        <h1>Welcome to the Page Editor!</h1>
-        <p class="text-white">When you first load the page editor, you'll have several options for customizing your
+        <h1 class="font-bold">Welcome to the Page Editor!</h1>
+        <p class="text-black">When you first load the page editor, you'll have several options for customizing your
             template:</p>
-        <ol class="text-white">
+        <ol >
             <li><strong class="text-yellow">Edit Text:</strong>
-                <div class="">Click on any text block to start editing it. Simply click on the text you want to
+                <div class="text-black">Click on any text block to start editing it. Simply click on the text you want to
                     modify, and start typing.</div>
             </li>
 
             <li><strong class="text-yellow">Choose Template Picture:</strong>
-                <div class="">Click on the <strong>"Choose"</strong> button to select a template picture for
+                <div class="text-black">Click on the <strong>"Choose"</strong> button to select a template picture for
                     your postcard. Browse and choose the one that best fits your message and style.</div>
             </li>
 
             <li><strong class="text-yellow">Color Pickers:</strong>
-                <div class=""> Use the color pickers to customize the background color and text color of your
+                <div class="text-black"> Use the color pickers to customize the background color and text color of your
                     postcard. Click on the color picker icons to open the color selection panel, and choose your desired
                     colors.</div>
             </li>
 
             <li><strong class="text-yellow">Rearrange Blocks:</strong>
-                <div class="">Click on the arrows to rearrange the blocks inside your postcard.</div>
+                <div class="text-black">Click on the arrows to rearrange the blocks inside your postcard.</div>
             </li>
 
             <li><strong class="text-yellow">Reset Button:</strong>
-                <div class="">Use the <strong>"Reset"</strong> button to reset any changes you've made to the
+                <div class="text-black">Use the <strong>"Reset"</strong> button to reset any changes you've made to the
                     picked colors and image. Clicking this button will revert the background color, text color, and template
                     picture back to their original settings.</div>
             </li>
 
             <li><strong class="text-yellow">Turn Off Button:</strong>
-                <div class="">Toggle the <strong>"Reset"</strong> button to hide or disable the current block.
+                <div class="text-black">Toggle the <strong>"Reset"</strong> button to hide or disable the current block.
                     This allows you to temporarily remove a block from your postcard without deleting it entirely.</div>
             </li>
         </ol>
@@ -55,7 +55,7 @@ export default{
    --secondary-yellow: #eec249; 
 }
 .text-yellow{
-    color: #eec249; 
+    color: #ffbb00; 
 }
 .container {
     max-width: 900px;
@@ -64,10 +64,13 @@ export default{
     padding: 20px;
     border-radius: 5px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-    background-color: #242424;
+    background-color: white;
     /* background-color: #1f1010; */
-    color: #eec249; 
+    color: #ffbb00; 
 }
+/* .test_class{
+    color : var(--secondary-yellow)
+} */
 
 h1 {
     font-size: 24px;
--- src/components/reasons_like_you_section.vue
+++ src/components/reasons_like_you_section.vue
@@ -41,7 +41,6 @@ export default {
         reader.onload = () => {
           this.image_url  = reader.result;
           var file_type = this.image_url.match('data:image/([a-zA-Z]+);')[1]
-          // var data_img_name = `./src/assets/imgs/user_input_${index}.${file_type}`
           this.$refs.img.setAttribute("data-verbose-path",`./assets/imgs/user_input_reasons_section.${file_type}`)
         };
         reader.readAsDataURL(file);
