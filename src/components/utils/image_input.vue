4a5fbf70fb9d20984e9a8edfd0cf513e9c4aaec8
unknown <alanuldashev@gmail.com>
2024-03-01 16:00:29 +0100

    image input components logic

--- src/components/utils/image_input.vue
+++ src/components/utils/image_input.vue
@@ -1,8 +1,8 @@
 <script>
 export default {
     props: {
-        image_url: Object,
-        reset: Boolean
+        image_url: String,
+        reset_img: Boolean
     },
     data() {
         return {
@@ -10,7 +10,7 @@ export default {
         }
     },
     watch: {
-        reset(newValue, oldValue) {
+        reset_img(newValue, oldValue) {
             if (newValue && !oldValue) {
                 this.user_custom_img = null
                 this.$emit('update:reset', false)
@@ -19,29 +19,30 @@ export default {
             }
         }
     },
-    handleFileUpload() {
-        this.$refs.file_input.click()
-    },
-    handleFileInputChange(event) {
-        const file = event.target.files[0]
-        if (file) {
-            const reader = new FileReader()
-            reader.onload = () => {
-                this.user_custom_img = reader.result
-                var file_type = this.image_url.match('data:image/([a-zA-Z]+);')[1]
-                this.$refs.img.setAttribute(
-                    'data-verbose-path',
-                    `./assets/imgs/user_input_like_you_section.${file_type}`
-                )
+    methods: {
+        handleFileUpload(event) {
+            const file = event.target.files[0]
+            console.log(file)
+            console.log('asdf')
+            if (file) {
+                const reader = new FileReader()
+                reader.onload = () => {
+                    this.user_custom_img = reader.result
+                        var file_type = this.user_custom_img.match('data:image/([a-zA-Z]+);')[1]
+                    this.$refs.img.setAttribute(
+                        'data-verbose-path',
+                        `./assets/imgs/user_input_like_you_section.${file_type}`
+                    )
+                }
+                reader.readAsDataURL(file)
             }
-            reader.readAsDataURL(file)
         }
     }
 }
 </script>
 
 <template>
-    <img @click="hadleFileUpload" :src="user_custom_img ? user_custom_img : image_url"
-        class="rounded-full aspect-square object-cover" ref="img" />
-    <input type="file" ref="file_input" class="hidden" @change="handleFileInputChange" accept="image/*" />
+    <img @click="$refs.file_input.click()" :src="user_custom_img ? user_custom_img : image_url"
+        class="rounded-full aspect-square object-cover" ref="img"/>
+    <input type="file" ref="file_input" class="hidden" @change="handleFileUpload" @click="$refs.file_input.value = null" accept="image/*" />
 </template>
