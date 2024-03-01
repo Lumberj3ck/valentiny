e707df5fdfb8e3776e2f3b465f24b053185766c1
unknown <alanuldashev@gmail.com>
2024-03-01 09:56:50 +0100

    new landing colors

--- src/components/landing_page.vue
+++ src/components/landing_page.vue
@@ -2,11 +2,12 @@
     <div class="md:absolute md:top-1/4 lg:top-[15%] xl:top-[5%] w-full">
     <div class=" flex flex-col md:flex-row-reverse ">
         <div class="flex justify-center md:w-5/6 lg:w-full">
-            <img src="../assets/imgs/composition.jpg" alt="composition" class="w-11/12 min-[540px]:w-3/4 lg:w-9/12">
+            <!-- <img src="../assets/imgs/composition.jpg" alt="composition" class="w-11/12 min-[540px]:w-3/4 lg:w-9/12"> -->
+            <img src="../assets/imgs/composition_1.png" alt="composition" class="w-11/12 min-[540px]:w-3/4 lg:w-9/12">
         </div>
         <div class="flex justify-center md:w-[65%] xl:w-3/4 items-center">
             <div class="flex flex-col w-5/6 max-w-sm min-[540px]:ml-[-50px] md:ml-0 md:max-w-md lg:max-w-[30rem]">
-                <div class="mb-4 lg:mb-10">
+                <div class="header mb-4 lg:mb-10">
                     <p class="font-bold text-2xl sm:text-3xl md:text-5xl mb-1 md:tracking-wide md:mb-2">
                         Listen to yourself.
                     </p>
@@ -14,7 +15,8 @@
                         Follow your heart
                     </p>
                 </div>
-                <p class="text-lg md:text-xl text-gray-600">
+                <!-- text-gray-600 -->
+                <p class="text-lg md:text-xl header">
                     Express your love with heartfelt messages on Special Day! Share your feelings, memories in your postcard. 
                 </p>
                 <div>
@@ -32,6 +34,10 @@
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
+.header{
+    color: var(--landing-text-color)
+}
+
 .landing_button{
     outline: none;
 }
@@ -41,10 +47,13 @@
 
 body {
   font-family: 'Poppins', sans-serif;
+  background-color: var(--landing-background-color);
 }
 
 .button_bg_color{
-    background-color: var(--land-button-yellow);
+    /* background-color: var(--land-button-yellow); */
+    background-color: var(--landing-button-color);
+    color: var(--landing-text-color);
 }
 
 
