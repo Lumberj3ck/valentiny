6977516750391d459dcfb67af6ff440341dce36e
unknown <alanuldashev@gmail.com>
2024-03-02 21:38:16 +0100

    aspect ration fix for safari

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,31 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    ({ matchUtilities, theme /* … */ }) => {
+      matchUtilities(
+        {
+          'aspect': (value) => ({
+            '@supports (aspect-ratio: 1 / 1)': {
+              aspectRatio: value,
+            },
+            '@supports not (aspect-ratio: 1 / 1)': {
+
+              '&::before': {
+                content: '""',
+                float: 'left',
+                paddingTop: `calc(100% / (${value}))`,
+              },
+              '&::after': {
+                clear: 'left',
+                content: '""',
+                display: 'block',
+              }
+            },
+          }),
+        },
+        { values: theme('aspectRatio') }
+      )
+    },
+  ],
 }
