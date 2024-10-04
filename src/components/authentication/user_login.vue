da617ce7c1c9c3f3cc63368e909f50633b0674f3
unknown <alanuldashev@gmail.com>
2024-10-04 21:35:01 +0200

    changed user login and register forms

--- src/components/authentication/user_login.vue
+++ src/components/authentication/user_login.vue
@@ -45,7 +45,7 @@
         Register
       </router-link>
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
+    <button type="submit" class="text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Submit</button>
   </form>
   <loading_spinner v-if="loading"></loading_spinner>
 </template>
--- src/components/authentication/user_register.vue
+++ src/components/authentication/user_register.vue
@@ -77,7 +77,7 @@
     </div>
     <button 
       type="submit"
-      class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+      class="text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:outline-none focus:ring-pink-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
     >
       Register new account
     </button>
