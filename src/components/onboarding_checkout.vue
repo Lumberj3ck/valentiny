59b0187ab82b3ccc236351e93033befde0f24630
unknown <alanuldashev@gmail.com>
2024-10-04 21:37:58 +0200

    stripe checkout component handles return url

---
+++ src/components/onboarding_checkout.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="max-w-screen-lg mx-auto mt-5">
+  <ol class="justify-center items-center w-full space-y-4 sm:flex sm:space-x-8 sm:space-y-0 rtl:space-x-reverse">
+      <li class="flex items-center text-pink-500  space-x-2.5 rtl:space-x-reverse">
+          <span class="flex items-center justify-center w-8 h-8 border border-pink-500 rounded-full shrink-0 ">
+              1
+          </span>
+          <span>
+              <h3 class="font-medium leading-tight">User info</h3>
+              <p class="text-sm">Register your account</p>
+          </span>
+      </li>
+      <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
+          <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+              2
+          </span>
+          <span>
+              <h3 class="font-medium leading-tight">Payment Info</h3>
+              <p class="text-sm">Pay for website publishing</p>
+          </span>
+      </li>
+      <li class="flex items-center text-gray-500 dark:text-gray-400 space-x-2.5 rtl:space-x-reverse">
+          <span class="flex items-center justify-center w-8 h-8 border border-gray-500 rounded-full shrink-0 dark:border-gray-400">
+              3
+          </span>
+          <span>
+              <h3 class="font-medium leading-tight">Publish website</h3>
+              <p class="text-sm">Choose domain name and publish </p>
+          </span>
+      </li>
+  </ol>
+</div>
+
+  <div>
+    <div v-if="currentStep === 1">
+      <user_register @register-success="nextStep" />
+    </div>
+    <div v-if="currentStep === 2">
+      <stripe_checkout @checkout-success="nextStep" />
+    </div>
+    <div v-if="currentStep === 3">
+      <website_publish @publish-success="nextStep" />
+    </div>
+  </div>
+</template>
+
+<script>
+import user_register from '@/components/authentication/user_register.vue'
+import website_publish from '@/components/website_publish.vue'
+import stripe_checkout from '@/components/stripe_checkout.vue'
+
+export default {
+  components: {
+    user_register,
+    website_publish,
+    stripe_checkout
+  },
+  data() {
+    return {
+      currentStep: 1
+    }
+  },
+  methods: {
+    nextStep() {
+      this.currentStep++
+    }
+  }
+}
+</script>
--- src/components/stripe_checkout.vue
+++ src/components/stripe_checkout.vue
@@ -1,27 +1,45 @@
 <template>
+  <div v-if="checkout_return">
+    <checkout_return />
+  </div>
+  <div class="flex justify-center mb-4">
     <router-link
       to="/page-editor/"
-      class="px-4 py-2 bg-gray-200 text-black rounded-md hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2"
-    >
-      Home
-    </router-link>
-  <div id="checkout"></div>
+      class="inline-flex items-center px-4 py-2 bg-gray-100 text-gray-700 rounded-md hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-300 transition-colors duration-300">
+      <i class="fas fa-arrow-left h-5 w-5 mr-2"></i>
+      Back to Editor
+      </router-link>
+    </div>
+    <div id="checkout"></div>
 </template>
 
 <script>
 import { loadStripe } from '@stripe/stripe-js'
 import { fetchClientSecret } from '@/js/api';
+import checkout_return from './checkout_return.vue';
 
 export default {
+  components: {
+    checkout_return
+  },
   data() {
     return {
       stripe: null,
       card: null,
       isLoading: false,
-      errorMessage: ''
+      errorMessage: '',
+      checkout_return: false
     }
   },
   async mounted() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const sessionId = urlParams.get('session_id');
+
+    if (sessionId) {
+      this.checkout_return = true
+      return;
+    }
+
     if (!this.stripe) {
       this.stripe = await loadStripe(
         'pk_test_51Ncm9BGG1bSL8LD4bWHztaALnFRI8weo3ahRLHnGOTOZk8KjzjJt1AVNVRpAIpp8crliJYI8KZZOHK8CclP1qv4T00oGmPV5Gv'
@@ -34,6 +52,14 @@ export default {
         fetchClientSecret
       })
       this.checkout.mount('#checkout')
+    }
+  },
+  watch: {
+    '$route.fullPath': {
+      handler(newPath) {
+        console.log('New URL:', newPath);
+      },
+      immediate: true
     }
   },
   unmounted() {
