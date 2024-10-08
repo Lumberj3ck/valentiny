2979b358cb91f40911f4bf2924301ac393fa5485
unknown <alanuldashev@gmail.com>
2024-10-08 13:33:29 +0200

    vercel analytics enabled

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import './assets/base.css'
 import './assets/main.css'
 import './assets/output.css'
 import { createPinia } from 'pinia'
+import { inject } from '@vercel/analytics';
 
 import { createApp } from 'vue/dist/vue.esm-bundler.js'
 // import { createApp } from 'vue'
@@ -18,3 +19,5 @@ app.use(createPinia())
 // app.component('FileUpload', FileUpload)
 
 app.mount('#app')
+
+inject();
